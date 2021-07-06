b137b137a45a733e86bf0ef94ded4c49a1838145
Beth Pauley <elizabeth.pauley@scotts.com>
2021-07-06 17:49:15 -0400

    Commit existing project files.

---
+++ script.js
@@ -0,0 +1,191 @@
+$(function() {
+    $( "#accordion" ).accordion(
+		{ active: 3 }
+	);
+  });
+  
+  $(function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  });
+  
+ $(function() {
+    $( "#dialog1" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener1" ).click(function() {
+      $( "#dialog1" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog2" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener2" ).click(function() {
+      $( "#dialog2" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog3" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener3" ).click(function() {
+      $( "#dialog3" ).dialog( "open" );
+    });
+  });
+  
+    $(function() {
+    $( "#dialog2" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener4" ).click(function() {
+      $( "#dialog2" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog3" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener5" ).click(function() {
+      $( "#dialog3" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog1" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener6" ).click(function() {
+      $( "#dialog1" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog3" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener7" ).click(function() {
+      $( "#dialog3" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog1" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener8" ).click(function() {
+      $( "#dialog1" ).dialog( "open" );
+    });
+  });
+  
+  $(function() {
+    $( "#dialog2" ).dialog({
+      autoOpen: false,
+	  height: 510,
+	  width: 632,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "fade",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener9" ).click(function() {
+      $( "#dialog2" ).dialog( "open" );
+    });
+  });
